d6a7b39a1ea19d85d2086f24ac9f5ae7c214bf56
architdas00001 <66167957+architdas00001@users.noreply.github.com>
2022-10-18 00:16:55 +0530

    Repo updated

--- src/main/java/com/cg/fitnesstracker/app/service/implementation/CustomerServiceImpl.java
+++ src/main/java/com/cg/fitnesstracker/app/service/implementation/CustomerServiceImpl.java
@@ -33,7 +33,7 @@ public class CustomerServiceImpl implements CustomerService{
 		if(cust!=null) {
 			List<Activity> activities = cust.getActivities();
 			activities.add(activity);
-			activities = customerRepository.saveAll(activities); 
+			activityRepository.saveAll(activities); 
 			return activities;
 		}
 		throw new RuntimeException("Can't update");
@@ -45,7 +45,7 @@ public class CustomerServiceImpl implements CustomerService{
 		if(cust!=null) {
 			List<Diet> diets = cust.getDiet();
 			diets.add(diet);
-			diets = customerRepository.saveAll(diets); 
+			dietRepository.saveAll(diets); 
 			return diets;
 		}
 		throw new RuntimeException("Can't update");
