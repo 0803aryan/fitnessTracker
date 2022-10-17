66b68c60515571123d2e91736cd2d42996d1181a
architdas00001 <66167957+architdas00001@users.noreply.github.com>
2022-10-18 00:32:22 +0530

    UserDto model

--- src/main/java/com/cg/fitnesstracker/app/service/AppUserService.java
+++ src/main/java/com/cg/fitnesstracker/app/service/AppUserService.java
@@ -1,6 +1,14 @@
 package com.cg.fitnesstracker.app.service;
 
+import java.util.List;
+
+import com.cg.fitnesstracker.app.model.Activity;
+import com.cg.fitnesstracker.app.model.Customer;
+import com.cg.fitnesstracker.app.model.Diet;
+
 public interface AppUserService {
+	public Customer updateCustomerEmailService(Customer customer, Boolean active);
+	public Customer updateCustomerPasswordService(Customer customer, Boolean active);
 	
 
 }
--- src/main/java/com/cg/fitnesstracker/app/service/implementation/AppUserServiceImpl.java
+++ src/main/java/com/cg/fitnesstracker/app/service/implementation/AppUserServiceImpl.java
@@ -1,7 +1,20 @@
 package com.cg.fitnesstracker.app.service.implementation;
 
+import com.cg.fitnesstracker.app.model.Customer;
 import com.cg.fitnesstracker.app.service.AppUserService;
 
 public class AppUserServiceImpl implements AppUserService{
+
+	@Override
+	public Customer updateCustomerEmailService(Customer customer, Boolean active) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer updateCustomerPasswordService(Customer customer, Boolean active) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
