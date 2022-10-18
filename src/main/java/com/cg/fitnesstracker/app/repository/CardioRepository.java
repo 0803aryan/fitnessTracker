c9bbc8ae13273ea4dfa6e846f11cca2b6304b16f
Raj0168 <54407392+Raj0168@users.noreply.github.com>
2022-10-18 10:23:29 +0530

    Update CardioRepository.java

--- src/main/java/com/cg/fitnesstracker/app/repository/CardioRepository.java
+++ src/main/java/com/cg/fitnesstracker/app/repository/CardioRepository.java
@@ -8,6 +8,6 @@ import com.cg.fitnesstracker.app.model.Cardio;
 import com.cg.fitnesstracker.app.model.enums.CardioType;
 
 public interface CardioRepository extends CrudRepository<Cardio, Integer>{
-	
-	
+
+	List<Cardio> findByCardioType(CardioType cardioType);
 }
